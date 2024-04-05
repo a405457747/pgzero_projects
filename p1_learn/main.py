bd2cf5df1c90c265ae9e980d31e237e9aeee31be
Sky.Allen <894982165@qq.com>
2024-04-05 14:54:50 +0800

    1

--- p1_learn/main.py
+++ p1_learn/main.py
@@ -2,6 +2,12 @@ import pgzrun
 player1 = Actor('1.png', [400, 300])
 player2 = Actor('2.png', [400, 250])
 
+def pObj(obj):
+    print(obj,type(obj));
+
+pObj(player1);
+pObj(player2);
+
 
 def draw():
     player1.draw()
